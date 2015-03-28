99e3d537ef71144b9247ffd0806d22c64ecf1586
Potfur <wachowski.michal@gmail.com>
2015-03-28 14:12:52 +0100

    refactored & improved tests

--- tests/Moss/Bridge/Extension/ResourceTest.php
+++ tests/Moss/Bridge/Extension/ResourceTest.php
@@ -17,24 +17,26 @@ class ResourceTest extends \PHPUnit_Framework_TestCase
 {
     private static $path;
 
+    private $placeholders = ['{bundle}' => 'test', '{directory}' => 'css'];
     private $resource;
     private $public;
 
     public static function setUpBeforeClass()
     {
         parent::setUpBeforeClass();
-        self::$path = __DIR__ . '/'.self::rand(8);
+        self::$path = __DIR__ . '/' . self::rand(8);
     }
 
     public function setUp()
     {
         $this->tearDown();
 
-        $this->resource = self::$path.'/resource/{bundle}/';
-        $this->public = self::$path.'/public/{bundle}/';
+        $this->resource = self::$path . '/resource/{bundle}/{directory}/';
+        $this->public = self::$path . '/public/{bundle}/{directory}/';
 
-        $path = strtr($this->resource, ['{bundle}' => 'test']);
-        if(!is_dir($path)) {
+        $path = strtr($this->resource, $this->placeholders);
+
+        if (!is_dir($path)) {
             mkdir($path, 0777, true);
         }
 
@@ -44,8 +46,8 @@ class ResourceTest extends \PHPUnit_Framework_TestCase
     protected static function rand($len, $chars = '1234567890qwertyuioplkjhgfdsazxcvbnm')
     {
         $str = '';
-        $max = strlen($chars)-1;
-        while($len--) {
+        $max = strlen($chars) - 1;
+        while ($len--) {
             $i = rand(0, $max);
             $str .= $chars[$i];
         }
@@ -55,7 +57,7 @@ class ResourceTest extends \PHPUnit_Framework_TestCase
 
     public function tearDown()
     {
-        if(!file_exists(self::$path)) {
+        if (!file_exists(self::$path)) {
             return;
         }
 
@@ -84,20 +86,38 @@ class ResourceTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $result);
     }
 
+    public function testBuild()
+    {
+        $resource = new Resource(true, $this->public, $this->resource);
+        $resource->build('test:css:style.css');
+
+        $public = strtr($this->public, $this->placeholders);
+        $resource = strtr($this->resource, $this->placeholders);
+
+        $this->assertFileExists($public . 'style.css');
+        $this->assertFileEquals($public . 'style.css', $resource . 'style.css');
+    }
+
     public function testResourceInTwig()
     {
         $options = [
-            'debug' => true,
-            'auto_reload' => true,
+            'debug' => false,
+            'auto_reload' => false,
             'strict_variables' => false,
             'cache' => false,
         ];
 
-        $loader = new \Twig_Loader_Array(['index.html' => '{% resource \'test:css:style.js\' %}']);
+        $loader = new \Twig_Loader_Array(['index.html' => '{% resource \'test:css:style.css\' %}']);
 
         $twig = new \Twig_Environment($loader, $options);
         $twig->setExtensions([new Resource(true, $this->public, $this->resource)]);
 
         $twig->render('index.html', ['numberOfApples' => 10]);
+
+        $public = strtr($this->public, $this->placeholders);
+        $resource = strtr($this->resource, $this->placeholders);
+
+        $this->assertFileExists($public . 'style.css');
+        $this->assertFileEquals($public . 'style.css', $resource . 'style.css');
     }
 }
